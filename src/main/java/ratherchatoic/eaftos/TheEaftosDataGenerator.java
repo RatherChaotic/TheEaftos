95f33a2377dea7b014f792315fb10f723d36dfe3
Aidan Spalding <OccationallyChaotic@outlook.com>
2024-11-19 23:41:22 -0500

    Initial commit

---
+++ src/main/java/ratherchatoic/eaftos/TheEaftosDataGenerator.java
@@ -0,0 +1,11 @@
+package ratherchatoic.eaftos;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheEaftosDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
